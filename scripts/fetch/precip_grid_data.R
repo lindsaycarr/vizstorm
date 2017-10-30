5ce5e3a48db59a403b11301daba64dd549de1bb5
Jordan S Read <jread@usgs.gov>
2017-10-30 10:02:52 -0500

    start on precip data fetcher

--- scripts/fetch/precip_grid_data.R
+++ scripts/fetch/precip_grid_data.R
@@ -3,5 +3,10 @@
 fetch.precip_grid_data <- function(viz){
   deps <- readDepends(viz)
   checkRequired(deps, c("grid"))
-  as(deps[['grid']], "Spatial")
+  grid <- deps[['grid']]
+  sp_grid <- as(grid, "Spatial")
+  sp_grid_WGS84 <- sp::spTransform(sp_grid, "+proj=longlat +datum=WGS84")
+  
+  stencil <- geoknife::simplegeom(sp_grid_WGS84)
+  warning('the rest is not implemented')
 }
