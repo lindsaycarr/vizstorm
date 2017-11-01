256a37e23c45102162e683a98c2b6866861b56e0
Jordan S Read <jread@usgs.gov>
2017-11-01 11:53:21 -0500

    fetch precip for part of #11

--- scripts/fetch/precip_grid_data.R
+++ scripts/fetch/precip_grid_data.R
@@ -8,5 +8,16 @@ fetch.precip_grid_data <- function(viz){
   sp_grid_WGS84 <- sp::spTransform(sp_grid, "+proj=longlat +datum=WGS84")
   
   stencil <- geoknife::simplegeom(sp_grid_WGS84)
-  warning('the rest is not implemented')
+  fabric_args <- viz[["webdata_args"]]
+  
+  fabric <- geoknife::webdata(
+    url = fabric_args$url, 
+    times = as.POSIXct(fabric_args$times), 
+    variables = fabric_args$variables
+  )
+  
+  job <- geoknife::geoknife(stencil, fabric, wait = TRUE, REQUIRE_FULL_COVERAGE=FALSE)
+  
+  data <- geoknife::result(job, with.units = TRUE)
+  saveRDS(data, file = viz[['location']])
 }
