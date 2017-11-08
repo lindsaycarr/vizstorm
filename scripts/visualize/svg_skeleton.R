99e437aa6a7eb42e16f69c7fb31a0bc9f9356c8a
Jordan S Read <jread@usgs.gov>
2017-11-08 11:31:38 -0600

    start on skeleton function

--- scripts/visualize/svg_skeleton.R
+++ scripts/visualize/svg_skeleton.R
@@ -1,4 +1,15 @@
 #' incomplete
 visualize.svg_skeleton <- function(viz){
+  deps <- readDepends(viz)
+  #initialize svg:
+  svg_root <- xml_new_root('svg') # and other details...TODO***
+  for (group_name in names(deps)){
+    group_list <- list(deps[[group_name]]) %>% setNames(group_name)
+    # this is not naming correctly...
+    svg_xml <- xml2::as_xml_document(group_list)
+    # now add to parent document:
+    
+  }
+  browser()
   stop('nothing here...')
 }
