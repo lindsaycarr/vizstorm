32744054abc39e1d289f0f1625b03b2d7a776a25
Lindsay Carr <lcarr@usgs.gov>
2018-01-04 11:13:56 -0600

    testing out js

---
+++ scripts/visualize/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var navigation = $('#navigation');
+    var menu = $('#menu');
+    
+    $(document).on('click', function(){
+      $(navigation).hide();
+    });
+    
+    $(menu).on('click', function(e){
+      e.stopPropagation();
+      $(navigation).toggle();
+    });
+  });
