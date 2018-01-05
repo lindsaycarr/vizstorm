59c4e2150bbc231b57e1ff06d2ec1c38da79beb1
Lindsay Carr <lcarr@usgs.gov>
2018-01-05 12:04:27 -0600

    remove commented code + edit comments

--- js/map.js
+++ js/map.js
@@ -6,7 +6,6 @@ var chart_height    =   600;
 var projection = d3.geoMercator() //changing to geoAlbers makes the map disappear
     .scale(1500)
     .center([-91.34397, 32.25196])
-    //.translate([0,0]);
     .translate([chart_width / 2, chart_height / 2]);
 var path = d3.geoPath()
     .projection(projection);
@@ -20,15 +19,8 @@ var svg = d3.select("body")
 var map = svg.append( 'g' )
     .attr( 'id', 'map' );
 
-// Data
+// Add map features
 d3.json('../cache/state_map.geojson', function(us_data){
-    
-    // us_data.features.forEach(function(d) {
-    //   d.geometry.coordinates[0][0] = parseFloat(d.geometry.coordinates[0][0]);
-    // });
-    
-    console.log(us_data);
-    //console.log(us_data.features[0].geometry.coordinates[0][0]);
 
     map.selectAll( 'path' )
         .data(us_data.features)
@@ -44,11 +36,9 @@ d3.json('../cache/state_map.geojson', function(us_data){
 });
 
 function mouseover(d){
-  // Highlight hovered province
-  d3.select(this).style('fill', 'orange');
+  d3.select(this).style('fill', 'orange'); 
 }
 
 function mouseout(d){
-  // Reset province color
   d3.select(this).style('fill', "cornflowerblue");
 }
